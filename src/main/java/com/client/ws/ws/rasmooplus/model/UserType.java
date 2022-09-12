1447cdeb5b547529c926fab6806069aa4a1f8a57
Alexandre Luiz dos Santos <alexandremcp@gmail.com>
2022-09-12 19:25:59 -0300

    Criando a entidade user_type

---
+++ src/main/java/com/client/ws/ws/rasmooplus/model/UserType.java
@@ -0,0 +1,18 @@
+package com.client.ws.ws.rasmooplus.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user_type")
+public class UserType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_type_id")
+    private long id;
+
+    private String name;
+
+    private String description;
+}
