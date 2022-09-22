998b51121702f1eca1e39f73583531206aa05f2f
Alexandre Luiz dos Santos <alexandremcp@gmail.com>
2022-09-21 21:54:49 -0300

    Tratando BadRequestException

---
+++ src/main/java/com/client/ws/ws/rasmooplus/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.client.ws.ws.rasmooplus.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/client/ws/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
+++ src/main/java/com/client/ws/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.client.ws.ws.rasmooplus.service.impl;
 
 import com.client.ws.ws.rasmooplus.dto.SubscriptionTypeDto;
+import com.client.ws.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.ws.rasmooplus.exception.NotFoundException;
 import com.client.ws.ws.rasmooplus.model.SubscriptionType;
 import com.client.ws.ws.rasmooplus.repository.SubscriptionTypeRepository;
@@ -8,7 +9,9 @@ import com.client.ws.ws.rasmooplus.service.SubscriptionTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.prefs.BackingStoreException;
 
 @Service
 public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
@@ -37,6 +40,9 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
 
     @Override
     public SubscriptionType create(SubscriptionTypeDto dto) {
+        if (Objects.nonNull(dto.getId())) {                          //Se o ID é não for nulo lança uma exceção, pois, não pode ser criado um novo SubscriptionType com um ID existente
+            throw new BadRequestException("Id deve ser nulo");      //Chama a exceção BadRequestException configurada no ResourceHandler
+        }
         return subscriptionTypeRepository.save(SubscriptionType.builder()
                 .id(dto.getId())
                 .name(dto.getName())
